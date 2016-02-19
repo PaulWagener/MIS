8eb52c08b0d59f5f0a73d56e312f72e8872174bf
Stino <Linksonder@gmail.com>
2016-02-19 16:25:44 +0100

    missing DbInitializer

---
+++ ModuleManager.DomainDAL/Utility/MyDbInitializer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModuleManager.DomainDAL.Utility
+{
+    public class MyDbInitializer : DropCreateDatabaseIfModelChanges<DomainContext>
+    {
+        protected override void Seed(DomainContext context)
+        {
+            base.Seed(context);
+
+           
+        }
+    }
+}
