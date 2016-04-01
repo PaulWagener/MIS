9f06e8f0fe5d95ec95b8538428fdb7b1011614ca
Stino <Linksonder@gmail.com>
2016-04-01 17:27:25 +0200

    docentcontroller erbij
    Related Work Items: #122, #123

---
+++ ModuleManager.Web/Controllers/Api/DocentController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using ModuleManager.DomainDAL;
+using ModuleManager.DomainDAL.Interfaces;
+using ModuleManager.Web.Controllers.Api.Interfaces;
+
+namespace ModuleManager.Web.Controllers.Api
+{
+    public class DocentController : ApiController
+    {
+        private readonly IGenericRepository<Docent> _docentRepository;
+
+        public DocentController(IGenericRepository<Docent> docentRepository)
+        {
+            _docentRepository = docentRepository;
+        }
+
+        [HttpGet, Route("api/Docent/Get")]
+        public IEnumerable<Docent> GetAll()
+        {
+            return _docentRepository.GetAll();
+        }
+
+        [HttpGet, Route("api/Docent/Get/{key}")]
+        //public Onderdeel GetOne(string key)
+        public Docent GetOne(int id)
+        {
+            var keys = new object[] { id };
+            return _docentRepository.GetOne(keys);
+        }
+
+        [HttpPost, Route("api/Docent/Delete")]
+        public void Delete(Docent entity)
+        {
+            _docentRepository.Delete(entity);
+        }
+
+        [HttpPost, Route("api/Docent/Edit")]
+        public void Edit(Docent entity)
+        {
+            _docentRepository.Edit(entity);
+        }
+
+        [HttpPost, Route("api/Docent/Create")]
+        public void Create(Docent entity)
+        {
+            _docentRepository.Create(entity);
+        }
+    }
+}
