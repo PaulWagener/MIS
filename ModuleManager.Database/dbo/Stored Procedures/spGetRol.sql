882d93afb474b64517ab161e9eb256f871500d82
_Install <_Install@NB160014>
2016-11-14 11:45:36 +0100

    Stored Procedures niet meer als post-deploy

---
+++ ModuleManager.Database/dbo/Stored Procedures/spGetRol.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE [dbo].[spGetRol]
+	@UserName varchar(100)
+AS
+Begin
+	Declare @ReturnRol varchar(10)
+
+	Begin
+		Select @ReturnRol = SysteemRol From [User] Where UserNaam = @UserName
+	End
+	
+	Select @ReturnRol
+End
+RETURN 0
